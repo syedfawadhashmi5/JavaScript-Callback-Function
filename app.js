a2ef6e95ab826de4456217d716aa11bc5d547fc4
syedfawadhashmi5 <59658569+syedfawadhashmi5@users.noreply.github.com>
2020-06-25 04:59:35 -0700

    Update app.js
    Create A CallBack Function Example

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-funcation person1 = (friend) => {
+const person = (fristperson, secondperson) => {
 
+    fristperson()
+    console.log("this is call back functuion");
+    secondperson()
 }
+
+
+const personone = () => {
+    console.log("Hi I am Frist Person");
+}
+
+
+
+
+
+const persontwo = () => {
+    console.log("Hi I am Second Person");
+}
+
+
+
+
+
+
+
+person(personone, persontwo);
